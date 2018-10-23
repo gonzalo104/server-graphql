2e2865efb9f9cc5a6e490cbd585f42a2c2a5abb9
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-10-23 18:57:34 -0500

    upload pro

---
+++ schema.js
@@ -0,0 +1,48 @@
+const {makeExecutableSchema} = require('graphql-tools');
+
+const typeDefs = `
+
+ #Esto es un curso en el sistema
+    type Curso {
+        id         : ID!
+        titulo     : String!
+        descripcion: String!
+        propfesor  : Profesor
+        rating     : Float
+        comentarios: [Comentario]
+    }
+
+    type Profesor{
+        id          : ID!
+        nombre      : String!
+        nacionalidad: String!
+        genero      : Genero
+        cursos      : [Curso]
+    }
+
+    enum Genero{
+        MASCULINO
+        FEMENINO
+    }
+
+    type Comentario{
+        id    : ID!
+        nombre: String!
+        cuerpo: String!
+    }
+
+    type Query{
+     cursos    : [Curso]
+     profesores: [Profesor]
+     
+     curso    (id: Int): Curso
+     profesor(id: Int) : Profesor
+    }
+
+`
+
+const schema = makeExecutableSchema({
+    typeDefs
+});
+
+module.exports = schema;
